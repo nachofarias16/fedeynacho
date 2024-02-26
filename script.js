f1be56437839af9763e5d6021f0e67387aa117bc
Fede <fedeprzy@gmail.com>
2024-02-26 19:25:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var login = document.getElementById('login');
 var formulario = document.getElementById('formulario');
 var close_login = document.getElementById('close-login');
 var sign_in = document.getElementById('sign_in');
+var nombre = document.getElementById('usuario');
+var password = document.getElementById('password');
 
 login.addEventListener('click', function(){
     formulario.style.display='block';
@@ -14,8 +16,10 @@ close_login.addEventListener('click', function(){
 });
 
 sign_in.addEventListener('click', function(){
-    var nombre = document.getElementById('usuario').value;
-    var password = document.getElementById('password').value;
-    console.log(nombre)
-    console.log(password)
+    username = nombre.value;
+    pass = password.value;
+    console.log(username);
+    console.log(pass);
+    nombre = nombre.value = "";
+    password = password.value = "";
 });
